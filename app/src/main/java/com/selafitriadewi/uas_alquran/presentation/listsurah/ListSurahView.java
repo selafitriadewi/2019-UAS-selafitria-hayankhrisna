3a6f3b71f8cc36e859117ab9cbd3166e8d829032
Hayan Khrisna <42838685+hayankhrisna@users.noreply.github.com>
2019-05-26 16:14:04 +0700

    Delete ListSurahView.java

--- app/src/main/java/com/selafitriadewi/uas_alquran/presentation/listsurah/ListSurahView.java
+++
@@ -1,8 +0,0 @@
-package com.selafitriadewi.uas_alquran.presentation.listsurah;
-
-
-import com.selafitriadewi.uas_alquran.base.BaseView;
-import com.selafitriadewi.uas_alquran.model.Surah;
-
-interface ListSurahView extends BaseView<Surah> {
-}
