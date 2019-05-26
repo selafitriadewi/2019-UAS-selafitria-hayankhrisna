9f7e8540e23cbbdc30607511226e21627494e827
selafitriadewi <fdewi1091@gmail.com>
2019-05-26 16:25:32 +0700

    list

---
+++ app/src/main/java/com/selafitriadewi/uas_alquran/presentation/listsurah/ListSurahActivity.java
@@ -0,0 +1,15 @@
+package com.selafitriadewi.uas_alquran.presentation.listsurah;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.selafitriadewi.uas_alquran.R;
+
+public class ListSurahActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_surah);
+    }
+}
